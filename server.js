d10a7d515bf8b18b07f357955e2275223e2749f7
Tosif <shekh.tosif@gmail.com>
2025-02-21 17:53:27 +0000

    server.js changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-
+const path = require('path')
 
 dotenv.config()
 
@@ -10,9 +10,15 @@ const app = express()
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//static files  
+app.use(express.static(path.join(__dirname,'./client/build')))
+
 //routes
 app.use('/api/v1/portfolio',require('./routes/portfolioRoute'))
-
+app.get('*',(req,res)=> {
+ res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 //port
 const PORT  = process.env.PORT || 8080
 //listen
