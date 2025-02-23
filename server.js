c0c6f00c8d8ada2060e802f13d43ff2d5d8eab31
Tosif <shekh.tosif@gmail.com>
2025-02-23 22:47:01 +0000

    server.js changes

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.use(cors())
 app.use(express.json())
 
 //static files  
-app.use(express.static(path.join(__dirname,'./client/build')))
+//app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'client', 'dist')))
 
 //routes
 app.use('/api/v1/portfolio',require('./routes/portfolioRoute'))
 app.get('*',(req,res)=> {
- res.sendFile(path.join(__dirname,'./client/build/index.html'))
+ res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'))
+ //res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 //port
 const PORT  = process.env.PORT || 8080
